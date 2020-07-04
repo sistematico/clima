a7b5cb7e0fe76b52b1811b8ffcb9afc64523a829
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-04 10:33:01 -0400

    Autocommit por GitHub.sh: alterações: add 'js/weather.js' add 'css/bulma.min.css'

--- js/weather.js
+++ js/weather.js
@@ -1,24 +1,11 @@
-// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
-// http://ip-api.com/json/IP?fields=status,city,lat,lon
-        
-// fetch('https://api.openweathermap.org/data/2.5/weather?q=Campo+Grande&units=metric&appid=' + apikey).then(function(response) {
-//     return response.json();
-// }).then(function(json) {
-//     document.querySelector('.clima').innerHTML = '<i class="fas fa-thermometer-half"></i> ' + json.main.temp + '°C';
-// });
-
-
-//fetch('http://ip-api.com/json/' + ip + '?fields=status,city,lat,lon').then(function(response) {
 fetch('https://ipapi.co/' + ip + '/json/').then(function(response) {
     return response.json();
 }).then(function(json) {
     let coords = { country: json.country, region: json.region, city: json.city, lat: json.latitude, lon: json.longitude };
 }).then(function(coords) {
-
     fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + coords.lat + '&lon=' + coords.lon + '&units=metric&appid=' + apikey).then(function(response) {
         return response.json();
     }).then(function(json) {
         document.querySelector('.clima').innerHTML = `<i class="fas fa-thermometer-half"></i> ${json.main.temp}°C em ${coords.city}, ${coords.region}`;
     });
-
 });
