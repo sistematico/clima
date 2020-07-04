cdcdd220126b3dc1f30455165b70e86a57631ee7
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-04 10:34:01 -0400

    Autocommit por GitHub.sh: alterações: add 'index.php'

--- index.php
+++ index.php
@@ -1,26 +1,22 @@
 <?php
-$config = include('cfg/config.php');
+	$config = include('cfg/config.php');
 ?>
-
-<!doctype html>
-<html lang="pt-br">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Clima Atual">
-	<meta name="author" content="Lucas Saliés Brum">
-	<title>Clima</title>
-	<meta property="og:title" content="Clima" />
-	<meta property="og:description" content="Clima Atual" />
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://clima.lucasbrum.net" />
-	<meta property="og:image" content="https://clima.lucasbrum.net/img/logo.png" />
-	<link rel="stylesheet" href="css/main.css">
-	<link rel="shortcut icon" href="img/favicon.ico">
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Clima</title>
+		<link rel="stylesheet" href="css/bulma.min.css">
 </head>
 <body>
-	<div class="clima"></div>
+	<section class="section">
+		<div class="container">
+			<p class="subtitle">
+				<div class="clima"></div>
+			</p>
+		</div>
+	</section>
 	<script>
 		const apikey = "<?php echo $config['api_key']; ?>";
 		const ip = "<?php echo $_SERVER['REMOTE_ADDR']; ?>";
