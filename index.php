975793ee85a1feeb599e98119513c78d3b5068f9
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-04 11:35:02 -0400

    Autocommit por GitHub.sh: alterações: add 'index.php'

--- index.php
+++ index.php
@@ -77,6 +77,14 @@
 			</nav>
 		</div>
 	</section>
+
+	<div class="modal">
+		<div class="modal-background"></div>
+  		<div class="modal-content">
+    		<!-- Any other Bulma elements you want -->
+  		</div>
+  		<button class="modal-close is-large" aria-label="close"></button>
+	</div>
 	<script>
 		const apikey = "<?php echo $config['api_key']; ?>";
 		const ip = "<?php echo $_SERVER['REMOTE_ADDR']; ?>";
