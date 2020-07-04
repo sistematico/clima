2f1ec1d3527f5370141b60857ece476b963fa753
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-04 10:10:01 -0400

    Autocommit por GitHub.sh: alterações: add 'js/weather.js'

---
+++ js/weather.js
@@ -0,0 +1,8 @@
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
+// http://ip-api.com/json/IP?fields=status,city,lat,lon
+        
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Campo+Grande&units=metric&appid=<?php echo $config["api_key"]; ?>').then(function(response) {
+    return response.json();
+}).then(function(json) {
+    document.querySelector('.clima').innerHTML = '<i class="fas fa-thermometer-half"></i> ' + json.main.temp + '°C';
+});
