bfbb9b7f1c5f52f6e872d5e427a26e733a7b9282
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-04 11:13:01 -0400

    Autocommit por GitHub.sh: alterações: add 'js/weather.js'

--- js/weather.js
+++ js/weather.js
@@ -8,5 +8,13 @@ fetch('https://ipapi.co/' + ip + '/json/').then(function(response) {
         return response.json();
     }).then(function(json) {
         document.querySelector('.clima').innerHTML = `<i class="fas fa-thermometer-half"></i> ${json.main.temp}°C em ${coord.city}, ${coord.region}`;
+
+        if (json.main.temp < 25) {
+            document.querySelector('.icone').src = 'img/frio.svg';
+        } else if (json.main.temp < 26) {
+            document.querySelector('.icone').src = 'img/legal.svg';
+        } else {
+            document.querySelector('.icone').src = 'img/calor.svg';
+        } 
     });
 });
