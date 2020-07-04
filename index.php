5cb6aa3bda5e941326443bbbb295cfcdba106e6e
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-04 10:07:01 -0400

    Autocommit por GitHub.sh: alterações: add 'index.php'

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
 $config = include('cfg/config.php');
 ?>
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Clima Atual">
+	<meta name="author" content="Lucas Saliés Brum">
+	<title>Clima</title>
+	<meta property="og:title" content="Clima" />
+	<meta property="og:description" content="Clima Atual" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://clima.lucasbrum.net" />
+	<meta property="og:image" content="https://clima.lucasbrum.net/img/logo.png" />
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="shortcut icon" href="img/favicon.ico">
+</head>
+<body>
+	corpo
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+	<script src="js/scripts.js"></script>
+</body>
+</html>
 <script>
 	// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
 	// http://ip-api.com/json/IP?fields=status,city,lat,lon
