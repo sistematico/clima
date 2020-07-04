8421e521e9bc45871e5b81534a7c8490c0489ef6
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-04 10:09:01 -0400

    Autocommit por GitHub.sh: alterações: add 'index.php'

--- index.php
+++ index.php
@@ -21,11 +21,9 @@ $config = include('cfg/config.php');
 </head>
 <body>
 	<div class="clima"></div>
+	<script>
+		const apikey = "<?php echo $config['api_key']; ?>";
+	</script> 
 	<script src="js/weather.js"></script>
 </body>
 </html>
-<script>
-	// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
-	// http://ip-api.com/json/IP?fields=status,city,lat,lon
-	const apikey = "<?php echo $config['api_key']; ?>";
-</script> 
