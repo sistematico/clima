9d6c3d037df045c1bbdcc3f5b61f89b7df7a8836
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-04 10:32:01 -0400

    Autocommit por GitHub.sh: alterações: add 'js/weather.js'

--- js/weather.js
+++ js/weather.js
@@ -9,10 +9,10 @@
 
 
 //fetch('http://ip-api.com/json/' + ip + '?fields=status,city,lat,lon').then(function(response) {
-fetch('http://ip-api.com/json/' + ip).then(function(response) {
+fetch('https://ipapi.co/' + ip + '/json/').then(function(response) {
     return response.json();
 }).then(function(json) {
-    let coords = { country: json.country, region: json.regionName, city: json.city, lat: json.lat, lon: json.lon };
+    let coords = { country: json.country, region: json.region, city: json.city, lat: json.latitude, lon: json.longitude };
 }).then(function(coords) {
 
     fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + coords.lat + '&lon=' + coords.lon + '&units=metric&appid=' + apikey).then(function(response) {
