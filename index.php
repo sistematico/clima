78da442843f42de6364e36b0c19dd95ac178daa2
Lucas Saliés Brum <lucas@archlinux.com.br>
2020-07-04 11:05:01 -0400

    Autocommit por GitHub.sh: alterações: add 'index.php'

--- index.php
+++ index.php
@@ -12,78 +12,59 @@
 </head>
 <body>
 	<section class="hero is-info is-large">
-  <div class="hero-head">
-    <nav class="navbar">
-      <div class="container">
-        <div class="navbar-brand">
-          <a class="navbar-item">
-            <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
-          </a>
-          <span class="navbar-burger burger" data-target="navbarMenuHeroB">
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
-        </div>
-        <div id="navbarMenuHeroB" class="navbar-menu">
-          <div class="navbar-end">
-            <a class="navbar-item is-active">
-              Home
-            </a>
-            <a class="navbar-item">
-              Examples
-            </a>
-            <a class="navbar-item">
-              Documentation
-            </a>
-            <span class="navbar-item">
-              <a class="button is-info is-inverted">
-                <span class="icon">
-                  <i class="fab fa-github"></i>
-                </span>
-                <span>Download</span>
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </div>
+		<div class="hero-head">
+			<nav class="navbar">
+				<div class="container">
+					<div class="navbar-brand">
+						<a class="navbar-item">
+							<!-- <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo"> -->
+							Clima
+						</a>
+						<span class="navbar-burger burger" data-target="navbarMenuHeroB">
+							<span></span>
+							<span></span>
+							<span></span>
+						</span>
+					</div>
+					<div id="navbarMenuHeroB" class="navbar-menu">
+						<div class="navbar-end">
+							<!-- <a class="navbar-item is-active">Home</a> -->
+							<span class="navbar-item">
+								<a href="https://github.com/sistematico/clima" class="button is-info is-inverted">
+									<span class="icon">
+										<i class="fab fa-github"></i>
+									</span>
+									<span>Download</span>
+								</a>
+							</span>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
 
-  <div class="hero-body">
-    <div class="container has-text-centered">
-      <p class="title clima"></p>
-      <p class="subtitle local"></p>
-    </div>
-  </div>
+		<div class="hero-body">
+			<div class="container has-text-centered">
+				<p class="title clima"></p>
+				<p class="subtitle local"></p>
+			</div>
+		</div>
 
-  <div class="hero-foot">
-    <nav class="tabs is-boxed is-fullwidth">
-      <div class="container">
-        <ul>
-          <li class="is-active">    <a>Overview</a>
-          </li>
-          <li>
-            <a>Modifiers</a>
-          </li>
-          <li>
-            <a>Grid</a>
-          </li>
-          <li>
-            <a>Elements</a>
-          </li>
-          <li>
-            <a>Components</a>
-          </li>
-          <li>
-            <a>Layout</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-</section>
-
+		<div class="hero-foot">
+			<nav class="tabs is-boxed is-fullwidth">
+				<div class="container">
+					<ul>
+						<li class="is-active"><a>Overview</a></li>
+						<li><a>Modifiers</a></li>
+						<li><a>Grid</a></li>
+						<li><a>Elements</a></li>
+						<li><a>Components</a></li>
+						<li><a>Layout</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</section>
 	<script>
 		const apikey = "<?php echo $config['api_key']; ?>";
 		const ip = "<?php echo $_SERVER['REMOTE_ADDR']; ?>";
