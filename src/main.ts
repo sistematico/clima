7bbe6c4415040d86eaacd2005efad3034a7cefe0
Lucas Saliés Brum <sistematico@gmail.com>
2024-10-24 01:40:43 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
+import '@/styles/main.scss'
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import App from '@/app.vue'
+import 'bootstrap'
 
 createApp(App).mount('#app')
