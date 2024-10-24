a54a398febf0b034ff85eba5f6136f8d08824cf0
Lucas Saliés Brum <sistematico@gmail.com>
2024-10-24 05:07:08 -0400

    Update

--- src/composables/forecast.ts
+++ src/composables/forecast.ts
@@ -1,4 +1,9 @@
 export async function useForecast(lat: string, lon: string) {
   const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,wind_speed_10m,rain,cloud_cover`
-  return await (await fetch(url)).json()
+
+  const headers = new Headers({
+    'Access-Control-Allow-Origin': window.location.href
+  })
+
+  return await (await fetch(url, { headers })).json()
 }
--- src/composables/geo.ts
+++ src/composables/geo.ts
@@ -1,11 +1,15 @@
 export async function useGeo(ip: string) {
   const url = `https://tools.keycdn.com/geo.json?host=${ip}`
   
+  // const headers = new Headers({
+  //   'Accept': 'application/json',
+  //   'Content-Type': 'application/json',
+  //   'User-Agent': `keycdn-tools:${window.location.href}`,
+  //   'Access-Control-Allow-Origin': window.location.href
+  // })
   const headers = new Headers({
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'User-Agent': `keycdn-tools:${window.location.href}`,
-    'Access-Control-Allow-Origin': window.location.href
+    'User-Agent': `keycdn-tools:${window.location.href}`
+    // 'Access-Control-Allow-Origin': window.location.href
   })
 
   return await (await fetch(url, { method: 'get', headers })).json()
