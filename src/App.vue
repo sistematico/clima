0e63e98afadf32a2ea7ae9913e15a2db02f1baa1
Lucas Saliés Brum <sistematico@gmail.com>
2024-10-24 03:41:53 -0400

    Update

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="container py-4 px-3 mx-auto">
-    <h1>Hello, Bootstrap and Vite!</h1>
-    <button class="btn btn-primary">Primary button</button>
-  </div>
-</template>
-
---
+++ src/app.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+<template>
+  <!-- <div> -->
+  <!-- <a href="https://vitejs.dev" target="_blank"> -->
+  <!-- <img src="/wheater.svg" class="logo" alt="Clima" /> -->
+  <!-- </a> -->
+  <!-- </div> -->
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <section class="section">
+    <div class="container">
+      <h1 class="title mb-3">Clima</h1>
+      <p class="subtitle mb-3">
+        Modern CSS framework based on
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">Flexbox</a>
+      </p>
+      <div class="field mb-3">
+        <div class="control">
+          <input class="input" type="text" placeholder="Input">
+        </div>
+      </div>
+      <div class="field mb-3">
+        <p class="control">
+          <span class="select">
+            <select>
+              <option>Select dropdown</option>
+            </select>
+          </span>
+        </p>
+      </div>
+      <div class="buttons">
+        <a class="button is-primary">Primary</a>
+        <a class="button is-link">Link</a>
+      </div>
+    </div>
+  </section>
+</template>
